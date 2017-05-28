25411455d8e6afa6b6120ec7317e3572d6ec4847
vagrant <vagrant@vagrant.vm>
2017-05-28 21:32:39 +0000

    Few updates to the databse model

--- database-setup.py
+++ database-setup.py
@@ -1,27 +1,26 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
 from sqlachemy.orm import sessionmaker, relationship
 from sqlachemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class Category(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+class Item(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(100))
     password_hash = Column(String(100))
-
-class Category(Base):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-
-class Item(Base):
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    description = Column(String(500), nullable=False)
 
 
 engine = create_engine('sqlite:///catalog.db')
